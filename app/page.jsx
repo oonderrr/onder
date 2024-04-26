08f42965826efd2966865091ec57ecabcce26197
onder <onder.bugdaci@gmail.com>
2024-04-26 08:59:22 +0300

    ana sayfa

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+import MyButton from "@/app/components/MyButton";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="h-screen w-full flex flex-col items-center px-5">
+        <h1 className="text-3xl font-bold">Projects</h1>
+        <div className="h-full w-full rounded-lg border border-black flex gap-2 p-2">
+          <MyButton
+            name={"Dice"}
+            href={"/"}
+          />
+        </div>
+      </div>
     </>
   );
 }
