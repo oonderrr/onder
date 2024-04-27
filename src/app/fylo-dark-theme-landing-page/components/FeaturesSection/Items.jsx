3ba337393c7b2b505e6ef7c810bf8d8a6e9f64e6
onder <onder.bugdaci@gmail.com>
2024-04-27 16:48:58 +0300

    Features section tamamlandı

---
+++ src/app/fylo-dark-theme-landing-page/components/FeaturesSection/Items.jsx
@@ -0,0 +1,31 @@
+import image1 from "/src/app/fylo-dark-theme-landing-page/ui/images/icon-access-anywhere.svg";
+import image2 from "/src/app/fylo-dark-theme-landing-page/ui/images/icon-security.svg";
+import image3 from "/src/app/fylo-dark-theme-landing-page/ui/images/icon-collaboration.svg";
+import image4 from "/src/app/fylo-dark-theme-landing-page/ui/images/icon-any-file.svg";
+
+export const Items = [
+  {
+    icon: image1,
+    title: "Access your files, anywhere",
+    description:
+      "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+  },
+  {
+    icon: image2,
+    title: "Security you can trust",
+    description:
+      "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.",
+  },
+  {
+    icon: image3,
+    title: "Real-time collaboration",
+    description:
+      "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
+  },
+  {
+    icon: image4,
+    title: "Store any type of file",
+    description:
+      "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+  },
+];
--- src/app/fylo-dark-theme-landing-page/page.jsx
+++ src/app/fylo-dark-theme-landing-page/page.jsx
@@ -1,5 +1,6 @@
 import Header from "@/src/app/fylo-dark-theme-landing-page/components/Header";
 import HeroSection from "@/src/app/fylo-dark-theme-landing-page/components/HeroSection";
+import FeaturesSection from "@/src/app/fylo-dark-theme-landing-page/components/FeaturesSection/FeaturesSection";
 
 function FyloDarkThemeLandingPage() {
   return (
@@ -7,6 +8,7 @@ function FyloDarkThemeLandingPage() {
       <div className="w-full h-max bg-fylo-dark-blue-main-background text-fylo-white flex flex-col items-center">
         <Header />
         <HeroSection />
+        <FeaturesSection />
       </div>
     </>
   );
