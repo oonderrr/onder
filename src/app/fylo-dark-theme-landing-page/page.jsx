1e7c27b4e05b0680c4da69967aa828aa9494b96f
onder <onder.bugdaci@gmail.com>
2024-04-27 17:19:53 +0300

    Stay Productive

--- src/app/fylo-dark-theme-landing-page/page.jsx
+++ src/app/fylo-dark-theme-landing-page/page.jsx
@@ -1,6 +1,7 @@
 import Header from "@/src/app/fylo-dark-theme-landing-page/components/Header";
 import HeroSection from "@/src/app/fylo-dark-theme-landing-page/components/HeroSection";
 import FeaturesSection from "@/src/app/fylo-dark-theme-landing-page/components/FeaturesSection/FeaturesSection";
+import StayProductive from "@/src/app/fylo-dark-theme-landing-page/components/StayProductive";
 
 function FyloDarkThemeLandingPage() {
   return (
@@ -9,6 +10,7 @@ function FyloDarkThemeLandingPage() {
         <Header />
         <HeroSection />
         <FeaturesSection />
+        <StayProductive />
       </div>
     </>
   );
