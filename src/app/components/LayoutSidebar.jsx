dd16d6fe3b7fdfd8256561409c2bb4f788a76f29
onder <onder.bugdaci@gmail.com>
2024-04-30 15:57:05 +0300

    saat ayarlandı

--- src/app/components/LayoutSidebar.jsx
+++ src/app/components/LayoutSidebar.jsx
@@ -1,7 +1,21 @@
+"use client";
 import Image from "next/image";
 import onder from "@/src/app/ui/img-onder.jpg";
+import {useEffect, useState} from "react";
 
 function LayoutSidebar() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+
+  const hours = time.getHours().toString().padStart(2, '0');
+  const minutes = time.getMinutes().toString().padStart(2, '0');
+
   return (
     <>
       <div>
@@ -107,7 +121,7 @@ function LayoutSidebar() {
             >
               <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm7-3.25v2.992l2.028.812a.75.75 0 0 1-.557 1.392l-2.5-1A.751.751 0 0 1 7 8.25v-3.5a.75.75 0 0 1 1.5 0Z" />
             </svg>
-            <p className="text-sm">17:39</p>
+            <p className="text-sm">{hours}:{minutes}</p>
           </div>
           <div className="flex gap-2">
             <svg
